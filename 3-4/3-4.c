81ef49c009e38534fc8a85a2a9d2fd2e47ca2f29
jsyeom01 <jsyeom01@gmail.com>
2023-12-03 00:47:52 +0900

    3-4 commit

---
+++ 3-4/3-4.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ROWS 3
+#define COLS 3
+
+void main(void) {
+	int A[ROWS][COLS] = { {1,1,1}, {2,2,2}, {3,3,3} };
+	int B[ROWS][COLS] = { {4,4,4}, {5,5,6}, {6,6,6} };
+	int C[ROWS][COLS];
+}
