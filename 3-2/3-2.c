32db7358cd664ba2b341ded4086179a5f7d9cb79
jsyeom01 <jsyeom01@gmail.com>
2023-12-03 00:47:19 +0900

    3-2 commit

---
+++ 3-2/3-2.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+
+void main()
+{
+	char str[100];
+	char ch1, ch2;
+	int i;
+
+	printf("여러 글자를 입력 : ");
+	gets(str);
+
+	printf("기존 문자와 새로운 문자 : ");
+	scanf("%c %c", &ch1, &ch2);
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		if (str[i] == ch1)
+			str[i] = ch2;
+	}
+	printf("변환된 결과 ==> %s \n", str);
+}
