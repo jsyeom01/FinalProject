78855cb6cb1701351bf57999dadb18486893794b
jsyeom01 <jsyeom01@gmail.com>
2023-12-03 00:44:55 +0900

    commit 2-6
    important file 2-6

---
+++ 2-6/2-6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+	int i, j, su, dan;
+	for (i = 0; i <= 2; i++) {
+		for (su = 1; su <= 9; su++) {
+			for (j = 1; j <= 3; j++) {
+				dan = 3 * i + j;
+				if (dan % 2 == 1) printf("%d * %d = %2d\t", dan, su, dan * su);
+				else printf("\t\t");
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+}
