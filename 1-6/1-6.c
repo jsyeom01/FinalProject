c88248d97a25dd80a68c5e76881bfd1a499be7bc
jsyeom01 <jsyeom01@gmail.com>
2023-12-03 00:38:41 +0900

    5 commit

---
+++ 1-6/1-6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(void) {
+	int month, days;
+	printf("달을 입력하사오 : ");
+	scanf_s("%d", &month);
+	switch (month) {
+	case 2:
+		days = 28; break;
+	case 4: case 6: case 9: case 11:
+		days = 30; break;
+	default:
+		days = 31; break;
+	}
+	printf("%d월의 일수는 %d입니다.\n", month, days);
+}
